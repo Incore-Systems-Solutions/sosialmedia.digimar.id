efdf70dff528c6058ab8caceae1a6c91110d3345
ekaardilahfebriyanti <ardilaheka@gmail.com>
2025-07-08 11:19:43 +0700

    init

---
+++ resources/lang/en/slider.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'slide_1_title' => 'Manage X, IG, WhatsApp, and More in One App',
+    'slide_1_desc' => 'Schedule posts, send WhatsApp messages, and boost engagement easily.',
+    'slide_1_cta' => 'Try the Demo',
+    'slide_2_cta' => 'Subscribe Now',
+
+    'slide_2_title' => 'Struggling to manage all your Social Media and WhatsApp accounts?',
+    'slide_2_desc' => 'Digimar helps you manage all social media and WhatsApp messages from one dashboard. No need to switch apps. All-in-one. You can even get help from AI.',
+
+    'slide_3_title' => 'socialmedia.digimar.id - One Hub for All Your Social Media',
+    'slide_3_desc' => 'Manage, schedule, and analyze all your social media accounts from one integrated dashboard with Digimar.',
+
+];
---
+++ resources/lang/zh/slider.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    'slide_1_title' => '一款应用管理X、IG、WhatsApp等多个平台',
+    'slide_1_desc' => '轻松安排帖子、发送WhatsApp消息并提升互动率。',
+    'slide_1_cta' => '试用演示',
+    'slide_2_cta' => '立即订阅',
+
+    'slide_2_title' => '难以管理所有社交媒体和WhatsApp账号？',
+    'slide_2_desc' => 'Digimar帮助您通过一个仪表板管理所有社交媒体和WhatsApp消息。无需频繁切换应用。一体化解决方案。您还可以获得AI的协助。',
+
+    'slide_3_title' => 'sosialmedia.digimar.id - 一站式社交媒体管理平台',
+    'slide_3_desc' => '使用Digimar，从一个集成式仪表板管理、排程和分析所有社交媒体账号。',
+
+];
